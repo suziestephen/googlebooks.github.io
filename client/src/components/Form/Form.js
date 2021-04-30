c238e58e4e3af37933528879288de8f2244413f3
suziestephen <suzietstephen@gmail.com>
2021-04-30 16:11:47 +1000

    adding in search form

---
+++ client/src/components/Form/Form.js
@@ -0,0 +1,27 @@
+import React from "react";
+
+
+function Form(props){
+return (
+  <div className="container">
+  <form>
+    <div className="form-group">
+      <input
+        onChange={props.handleInputChange}
+        value={props.search}
+        name="search"
+        type="text"
+        className="form-control"
+        placeholder="Search a Book"
+        id="search"
+      />
+      <button onClick={props.handleFormSubmit} className="btn btn-dark mt-3 mb-5">
+        Search
+        </button>
+    </div>
+  </form>
+  </div>
+);
+}
+
+export default Form;
