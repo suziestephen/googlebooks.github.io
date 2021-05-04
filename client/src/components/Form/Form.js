a5ac188b424b7390b70b2fdf674b46cd77d1e0b8
suziestephen <suzietstephen@gmail.com>
2021-05-05 08:31:12 +1000

    adding header text

--- client/src/components/Form/Form.js
+++ client/src/components/Form/Form.js
@@ -6,6 +6,7 @@ return (
   <div className="container">
   <form>
     <div className="form-group">
+      <label htmlFor="search"><h2>Search for and save Books of Interest</h2></label>
       <input
         onChange={props.handleInputChange}
         value={props.search}
